28895aa1a78674ba2c6cc3e198cf61627abab79f
pramilasree <161211152+pramilasree@users.noreply.github.com>
2024-02-28 17:25:48 +0530

    Add files via upload

---
+++ daytosecond.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class daytosecond{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int min,sec,time;
+        int days=scan.nextInt();
+        for(int i=days;i<=days;i++){
+            time=days*12;
+            min=time*60;
+            sec=min*60;
+     System.out.println(sec);
+        } 
+    }
+}
